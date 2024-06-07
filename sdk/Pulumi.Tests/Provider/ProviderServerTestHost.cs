97aac8120435019b96b76c44be79dad1e3d85e51
AlgoMinds - Martin Plücker <martin.pluecker@algominds.de>
2024-06-07 12:08:41 +0200

    some refactoring in preparation of provider tests

---
+++ sdk/Pulumi.Tests/Provider/ProviderServerTestHost.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Pulumi.Tests.Provider;
+
+public class ProviderServerTestHost<TProvider> : IAsyncLifetime where TProvider : Experimental.Provider.Provider
+{
+    private IHost? host;
+    private GrpcChannel? channel;
+
+    public IHost Host => host ?? throw new InvalidOperationException("Host is not running");
+    public GrpcChannel Channel => channel ?? throw new InvalidOperationException("Host is not running");
+
+    public async Task InitializeAsync()
+    {
+        var tcpListener = System.Net.Sockets.TcpListener.Create(0);
+        var args = new string[] { tcpListener.LocalEndpoint.ToString() };
+
+        var cts = new System.Threading.CancellationTokenSource();
+
+        // Custom stdout so we can see what port Serve chooses
+        host = Experimental.Provider.Provider.BuildHost(args, "1.0",
+            _ => Activator.CreateInstance<TProvider>() ??
+                 throw new InvalidOperationException($"Unable to create instance of type '{typeof(TProvider).Name}'."));
+        await host.StartAsync(cts.Token);
+
+        // Grab the port from stdout and create a connection to it
+        var hostUri = Experimental.Provider.Provider.GetHostUri(host);
+
+        // Inititialize the engine channel once for this address
+        channel = GrpcChannel.ForAddress(hostUri, new GrpcChannelOptions
+        {
+            Credentials = Grpc.Core.ChannelCredentials.Insecure,
+        });
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (channel != null)
+        {
+            await channel.ShutdownAsync();
+            channel.Dispose();
+            channel = null;
+        }
+
+        if (host != null)
+        {
+            await host.StopAsync();
+            host.Dispose();
+            host = null;
+        }
+    }
+}

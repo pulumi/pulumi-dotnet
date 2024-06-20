86bb2829f78be13c095838f7556d0c4991e4df95
AlgoMinds - Martin Plücker <martin.pluecker@algominds.de>
2024-06-20 09:12:18 +0200

    add missing classes

---
+++ sdk/Pulumi/Deployment/GrpcDeploymentBuilder.cs
@@ -0,0 +1,20 @@
+﻿namespace Pulumi;
+
+internal class GrpcDeploymentBuilder : IDeploymentBuilder
+{
+    public static GrpcDeploymentBuilder Instance = new();
+
+    private GrpcDeploymentBuilder()
+    {
+    }
+
+    public IEngine BuildEngine(string engineAddress)
+    {
+        return new GrpcEngine(engineAddress);
+    }
+
+    public IMonitor BuildMonitor(string monitoringEndpoint)
+    {
+        return new GrpcMonitor(monitoringEndpoint);
+    }
+}
---
+++ sdk/Pulumi/Deployment/IDeploymentBuilder.cs
@@ -0,0 +1,7 @@
+﻿namespace Pulumi;
+
+internal interface IDeploymentBuilder
+{
+    public IEngine BuildEngine(string engineAddress);
+    public IMonitor BuildMonitor(string monitoringEndpoint);
+}

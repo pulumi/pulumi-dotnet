bdff53b66c3d348e2abcd814db2ac5071b2f7bb5
AlgoMinds - Martin Plücker <martin.pluecker@algominds.de>
2024-06-26 15:54:08 +0200

    add CustomTimeouts serialization roundtrip

--- sdk/Pulumi.Tests/Resources/CustomTimeoutsTests.cs
+++ sdk/Pulumi.Tests/Resources/CustomTimeoutsTests.cs
@@ -22,5 +22,22 @@ namespace Pulumi.Tests.Resources
             Assert.Equal(TimeSpan.FromHours(1) + TimeSpan.FromMinutes(3) + TimeSpan.FromMilliseconds(12.99) + TimeSpan.FromMilliseconds(1.0 / 1000.0) + TimeSpan.FromMilliseconds(100.0 / 1000_000.0), timeouts.Update);
             Assert.Null(timeouts.Delete);
         }
+
+        [Fact]
+        public void TestSerializationRoundtrip()
+        {
+            var timeouts = new CustomTimeouts()
+            {
+                Create = TimeSpan.FromHours(1) + TimeSpan.FromMinutes(3) + TimeSpan.FromMilliseconds(12.99) + TimeSpan.FromMilliseconds(1.0 / 1000.0) + TimeSpan.FromMilliseconds(100.0 / 1000_000.0),
+                Update = TimeSpan.FromTicks(123456789),
+                Delete = TimeSpan.FromHours(1) + TimeSpan.FromMilliseconds(100.0 / 1000_000.0)
+            };
+            var serialized = timeouts.Serialize();
+            var deserialized = CustomTimeouts.Deserialize(serialized);
+
+            Assert.Equal(timeouts.Create, deserialized.Create);
+            Assert.Equal(timeouts.Update, deserialized.Update);
+            Assert.Equal(timeouts.Delete, deserialized.Delete);
+        }
     }
 }

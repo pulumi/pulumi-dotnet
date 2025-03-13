d23b959a02ae59136d203b93766a9b7728265681
Hossein Margani <Hossein.Margani@towerswatson.com>
2025-03-13 13:21:43 +0000

    Simplify argument addition for --show-reads in WorkspaceStack

--- sdk/Pulumi.Automation/WorkspaceStack.cs
+++ sdk/Pulumi.Automation/WorkspaceStack.cs
@@ -333,10 +333,7 @@ namespace Pulumi.Automation
                     args.Add("--expect-no-changes");
 
                 if (options.ShowReads.HasValue)
-                {
-                    args.Add("--show-reads");
-                    args.Add(options.ShowReads.Value ? "true" : "false");
-                }
+                    args.Add($"--show-reads={options.ShowReads.Value ? "true" : "false"}");
 
                 if (options.Diff is true)
                     args.Add("--diff");

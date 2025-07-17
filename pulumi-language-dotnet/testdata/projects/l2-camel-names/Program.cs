75c8e7a7bb1f12ed76a3658abe3f67738cc4e7dd
Julien Poissonnier <julien@caffeine.lu>
2025-07-17 15:20:05 +0200

    Bump pulumi submodule to b3e608c850

---
+++ pulumi-language-dotnet/testdata/projects/l2-camel-names/Program.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+using Pulumi;
+using CamelNames = Pulumi.CamelNames;
+
+return await Deployment.RunAsync(() => 
+{
+    var firstResource = new CamelNames.CoolModule.SomeResource("firstResource", new()
+    {
+        TheInput = true,
+    });
+
+    var secondResource = new CamelNames.CoolModule.SomeResource("secondResource", new()
+    {
+        TheInput = firstResource.TheOutput,
+    });
+
+});
+

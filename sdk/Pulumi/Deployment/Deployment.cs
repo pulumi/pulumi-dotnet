a741ac31412cd373b340bd53bd880d42a33bc0f2
Brandon Pollack <brandonpollack23@gmail.com>
2025-08-05 05:17:27 +0900

    Add conformance for rootDirectory pcl function (#466)
    Add conformance for projectRoot pcl function
    
    Part of pulumi/pulumi#18565
    Blocked by pulumi/pulumi#18595
    
    Changes required to get project root in dotnet and have conformance
    tests pass.
    
    ---------
    
    Co-authored-by: Fraser Waters <fraser@pulumi.com>

--- sdk/Pulumi/Deployment/Deployment.cs
+++ sdk/Pulumi/Deployment/Deployment.cs
@@ -98,6 +98,7 @@ namespace Pulumi
 
         private readonly string _organizationName;
         private readonly string _projectName;
+        private readonly string? _rootDirectory;
         private readonly string _stackName;
         private readonly bool _isDryRun;
         private readonly ConcurrentDictionary<string, bool> _featureSupport = new ConcurrentDictionary<string, bool>();
@@ -121,6 +122,7 @@ namespace Pulumi
             var monitor = Environment.GetEnvironmentVariable("PULUMI_MONITOR");
             var engine = Environment.GetEnvironmentVariable("PULUMI_ENGINE");
             var project = Environment.GetEnvironmentVariable("PULUMI_PROJECT");
+            var rootDirectory = Environment.GetEnvironmentVariable("PULUMI_ROOT_DIRECTORY");
             var organization = Environment.GetEnvironmentVariable("PULUMI_ORGANIZATION");
             var stack = Environment.GetEnvironmentVariable("PULUMI_STACK");
             var pwd = Environment.GetEnvironmentVariable("PULUMI_PWD");
@@ -144,6 +146,7 @@ namespace Pulumi
             _isDryRun = dryRunValue;
             _stackName = stack;
             _projectName = project;
+            _rootDirectory = rootDirectory;
             _organizationName = organization ?? "organization";
 
             var deploymentLogger = CreateDefaultLogger();
@@ -173,6 +176,7 @@ namespace Pulumi
             _isDryRun = options?.IsPreview ?? true;
             _stackName = options?.StackName ?? "stack";
             _projectName = options?.ProjectName ?? "project";
+            _rootDirectory = options?.RootDirectory;
             _organizationName = options?.OrganizationName ?? "organization";
             this.Engine = engine;
             this.Monitor = monitor;
@@ -182,6 +186,19 @@ namespace Pulumi
 
         string IDeployment.OrganizationName => _organizationName;
         string IDeployment.ProjectName => _projectName;
+        string IDeployment.RootDirectory
+        {
+            get
+            {
+                if (_rootDirectory == null)
+                {
+                    throw new InvalidOperationException("The Pulumi CLI version does not support root directory. Please update the Pulumi CLI.");
+                }
+
+                return _rootDirectory;
+            }
+        }
+
         string IDeployment.StackName => _stackName;
         bool IDeployment.IsDryRun => _isDryRun;
 

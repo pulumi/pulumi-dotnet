f1e63b3bbb7c307bdca7be54d3c2902d02e9f5ec
Ian Wahbe <me@iwahbe.com>
2025-10-06 14:09:51 +0200

    Check in files for `l2-resource-option-hide-diffs`

---
+++ pulumi-language-dotnet/testdata/projects/l2-resource-option-hide-diffs/Program.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+using Pulumi;
+using Simple = Pulumi.Simple;
+
+return await Deployment.RunAsync(() => 
+{
+    var hideDiffs = new Simple.Resource("hideDiffs", new()
+    {
+        Value = true,
+    }, new CustomResourceOptions
+    {
+        IgnoreChanges =
+        {
+            "@value",
+        },
+    });
+
+    var notHideDiffs = new Simple.Resource("notHideDiffs", new()
+    {
+        Value = true,
+    });
+
+});
+

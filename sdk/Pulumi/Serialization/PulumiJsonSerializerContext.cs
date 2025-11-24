863c26756d592b0d3176f2c739f25f2c16cb07cb
Dylan Vos <dylan@vos.email>
2025-11-24 10:36:09 +0100

    Added missed `JsonSerializer.Serialize`

--- sdk/Pulumi/Serialization/PulumiJsonSerializerContext.cs
+++ sdk/Pulumi/Serialization/PulumiJsonSerializerContext.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace Pulumi.Serialization;
@@ -5,6 +6,7 @@ namespace Pulumi.Serialization;
 [JsonSourceGenerationOptions(WriteIndented = true)]
 [JsonSerializable(typeof(Output<int>))]
 [JsonSerializable(typeof(Output<string>))]
+[JsonSerializable(typeof(IEnumerable<string>))]
 internal partial class PulumiJsonSerializerContext : JsonSerializerContext
 {
 

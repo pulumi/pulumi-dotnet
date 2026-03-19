0df0e2c61a71422e684482bd8b82cdfc29248fd7
Ian Wahbe <me@iwahbe.com>
2026-03-19 22:07:17 +0100

    Unskip l1-builtin-cwd (#915)
    Generate testdata snapshot for l1-builtin-cwd and remove it from the
    expected failures list.

---
+++ pulumi-language-dotnet/testdata/projects/l1-builtin-cwd/Program.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Pulumi;
+
+return await Deployment.RunAsync(() => 
+{
+    return new Dictionary<string, object?>
+    {
+        ["cwdOutput"] = Directory.GetCurrentDirectory(),
+    };
+});
+

8f776e4c4f808fb7383aa6395cc8b6342efa67af
Fraser Ashyggton <fraser@pulumi.com>
2026-05-05 10:37:20 +0100

    Unskip l1-output-number and l1-config-types-primitive (#986)
    Seems these tests already works

--- pulumi-language-dotnet/testdata/projects/l1-config-types-primitive/Program.cs
+++ pulumi-language-dotnet/testdata/projects/l1-config-types-primitive/Program.cs
@@ -6,7 +6,9 @@ return await Deployment.RunAsync(() =>
 {
     var config = new Config();
     var aNumber = config.RequireDouble("aNumber");
-    var optionalNumber = config.GetDouble("optionalNumber") ?? 41;
+    var optionalNumber = config.GetDouble("optionalNumber") ?? 41.5;
+    var anInt = config.RequireInt32("anInt");
+    var optionalInt = config.GetInt32("optionalInt") ?? 1;
     var aString = config.Require("aString");
     var optionalString = config.Get("optionalString") ?? "defaultStringValue";
     var aBool = config.RequireBoolean("aBool");
@@ -14,7 +16,9 @@ return await Deployment.RunAsync(() =>
     return new Dictionary<string, object?>
     {
         ["theNumber"] = aNumber + 1.25,
-        ["defaultNumber"] = optionalNumber + 1,
+        ["defaultNumber"] = optionalNumber + 1.2,
+        ["theInteger"] = anInt + 4,
+        ["defaultInteger"] = optionalInt + 2,
         ["theString"] = $"{aString} World",
         ["defaultString"] = optionalString,
         ["theBool"] = !aBool && true,
